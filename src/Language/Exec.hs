4f223b75a82b3e21e47f493c7e76d53b6eb5d2de
Kristijan Biscanic <kbiscanic@gmail.com>
2015-01-18 12:20:09 -0800

    some stuff

--- src/Language/Exec.hs
+++ src/Language/Exec.hs
@@ -1,5 +1,8 @@
 module Language.Exec where
 
+import qualified Data.Map             as M
+import           Language.Expressions
+
 -- A model of a command which is waiting for arguments and a state to run
 type Command = [String] -> ScriptState -> IO ScriptState
 
@@ -10,16 +13,17 @@ type VarTable = M.Map String String
 type CommandTable = M.Map String Command
 -- A script state containing the last output, current working directory and
 -- the current table of variables.
-data ScriptState = ScriptState { output :: String
-, wd :: FilePath
-, vartable :: VarTable
-} deriving Show
+data ScriptState = ScriptState { output   :: String
+                               , wd       :: FilePath
+                               , vartable :: VarTable
+                               } deriving Show
 -- Runs a set of commands for a given command table. If this is the first
 -- command in the chain, it is given a FilePath and constructs a new, initially
 -- blank, ScriptState. Otherwise, it is given the state as left by the previous
 -- command’s execution.
-runHashProgram :: CommandTable -> Either FilePath ScriptState -> [TLExpr]
--> IO ScriptState
+runHashProgram :: CommandTable -> Either FilePath ScriptState -> [TLExpr] -> IO ScriptState
+runHashProgram cmds (Left wd) xs = runHashProgram cmds (ScriptState {output = "", wd = w, vartable = M.empty}) xs
+
 -- Calculates the result of a top-level command execution
 runTopLevel :: CommandTable -> ScriptState -> TLExpr -> IO ScriptState
 -- The rest of the module should consist of similar functions, calling each
